5a2a8be784daeb83573986dc101a2578793a8679
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2010-10-20 22:40:35 +0200

    Added retries to the SSHTunnel class.

--- lib/shared/ssh_tunnel.rb
+++ lib/shared/ssh_tunnel.rb
@@ -8,17 +8,29 @@ class SSHTunnel
   end
   
   def open
-    Thread.new do
-      Net::SSH.start(@host, @user) do |ssh|
-        ssh.forward.local(@local_port, 'localhost', 2288)
-        ssh.loop { @up = true }
-      end
-    end
+    connect
     
+    start_time = Time.now
     while true
       break if @up
       sleep 0.5
+      
+      if Time.now - start_time > 5
+        puts "SSH connection failed, trying again..."
+        start_time = Time.now
+        connect
+      end
     end
+  end
+  
+  def connect
+    @thread.kill! if @thread
+    @thread = Thread.new do
+      Net::SSH.start(@host, @user, { :timeout => 1 }) do |ssh|
+        ssh.forward.local(@local_port, 'localhost', 2288)
+        ssh.loop {  @up = true }
+      end
+    end    
   end
   
 end
