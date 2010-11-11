7e2e1567a37a65d90d6d934b3c5eb6372928ab6f
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2010-11-11 20:37:58 +0100

    Added a generator for rails 3.

---
+++ lib/generators/testbot/testbot_generator.rb
@@ -0,0 +1,13 @@
+class TestbotGenerator < Rails::Generators::Base
+  source_root File.expand_path('../templates', __FILE__)
+  
+  argument :project, :type => :string, :default => 'project'
+  class_option :connect, :type => :string, :default => "server", :desc => "Which server to use (required)"
+  class_option :server_path, :type => :string, :default => nil, :desc => "Sync path on the server (default: #{Testbot::DEFAULT_SERVER_PATH})"  
+  class_option :ignores, :type => :string, :default => nil, :desc => "Files to ignores when syncing (default: include all)"
+  class_option :ssh_tunnel, :type => :string, :default => nil, :desc => "Use a ssh tunnel (format: user@server)"
+
+  def generate_config
+    template "testbot.yml.erb", "config/testbot.yml"
+  end
+end
--- lib/railtie.rb
+++ lib/railtie.rb
@@ -3,8 +3,6 @@ require 'rails'
 
 module Testbot
   class Railtie < Rails::Railtie
-    railtie_name :testbot
-
     rake_tasks do
       load "tasks/testbot.rake"
     end
