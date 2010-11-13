b340e70fff78cf105767042a451e35342c48e1c8
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2010-11-13 14:49:40 +0100

    Most of the rails 3 generator done.

--- lib/generators/testbot/testbot_generator.rb
+++ lib/generators/testbot/testbot_generator.rb
@@ -5,7 +5,8 @@ class TestbotGenerator < Rails::Generators::Base
   class_option :connect, :type => :string, :required => true, :desc => "Which server to use (required)"
   class_option :rsync_path, :type => :string, :default => nil, :desc => "Sync path on the server (default: #{Testbot::DEFAULT_SERVER_PATH})"  
   class_option :rsync_ignores, :type => :string, :default => nil, :desc => "Files to rsync_ignores when syncing (default: include all)"
-  class_option :ssh_tunnel, :type => :string, :default => nil, :desc => "Use a ssh tunnel (format: user@server)"
+  class_option :ssh_tunnel, :type => :boolean, :default => nil, :desc => "Use a ssh tunnel"
+  class_option :user, :type => :string, :default => nil, :desc => "Use a custom rsync / ssh tunnel user (default: #{Testbot::DEFAULT_USER})"
 
   def generate_config
     template "testbot.yml.erb", "config/testbot.yml"
