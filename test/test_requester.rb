84f9455b400e1ca99b44e3a4ea09b150c8e90457
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2010-11-24 22:06:03 +0100

    Another attempt at getting CI to go green.

--- test/test_requester.rb
+++ test/test_requester.rb
@@ -291,10 +291,11 @@ class RequesterTest < Test::Unit::TestCase
       ENV['USE_JRUBY'] = "true"
       requester = Requester.new
       flexmock(requester).should_receive(:system)
+      flexmock(Mac).should_receive(:addr).and_return("00:01:..") 
 
       other_args = { :type=>"test", :available_runner_usage=>"100%",
         :root=>"testbot@:/tmp/testbot/jocke", :files=>"test/some_test.rb",
-        :requester_mac=>Mac.addr, :sizes=>"0", :project=>"project" }
+        :requester_mac=>"00:01:..", :sizes=>"0", :project=>"project" }
 
       flexmock(requester).should_receive(:find_tests).and_return([ 'test/some_test.rb' ])
       flexmock(HTTParty).should_receive(:post).with(any, :body => other_args.merge({ :jruby => true })).and_return('5')
