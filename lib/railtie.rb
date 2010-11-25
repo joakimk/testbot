87a1b0cc37657f88b3d2e072c88538aa9cb527cd
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2010-11-25 23:36:54 +0100

    Bugfix.

--- lib/railtie.rb
+++ lib/railtie.rb
@@ -1,10 +1,10 @@
 require 'testbot'
-require 'rails'
-
-module Testbot
-  class Railtie < Rails::Railtie
-    rake_tasks do
-      load File.expand_path(File.join(File.dirname(__FILE__), "tasks/testbot.rake"))
+if require('rails') rescue nil
+  module Testbot
+    class Railtie < Rails::Railtie
+      rake_tasks do
+        load File.expand_path(File.join(File.dirname(__FILE__), "tasks/testbot.rake"))
+      end
     end
   end
 end
