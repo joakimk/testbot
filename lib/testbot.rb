10405bfbd53207174be4b10ac47a2ba7746dbb6b
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2010-11-28 18:10:56 +0100

    Prep for release.

--- lib/testbot.rb
+++ lib/testbot.rb
@@ -8,7 +8,7 @@ unless defined?(Testbot)
     require 'railtie' if defined?(Rails)
 
     # Don't forget to update readme and changelog
-    VERSION = "0.3.5"
+    VERSION = "0.3.6"
 
     SERVER_PID = "/tmp/testbot_server.pid"
     RUNNER_PID = "/tmp/testbot_runner.pid"
