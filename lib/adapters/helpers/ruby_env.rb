2768237911b23e4c81866836888475ab7a751703
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2010-12-02 20:47:01 +0100

    Added ruby_command contruction in RubyEnv. refs #17.

--- lib/adapters/helpers/ruby_env.rb
+++ lib/adapters/helpers/ruby_env.rb
@@ -4,4 +4,15 @@ class RubyEnv
     Gem.available?("bundler") && File.exists?("#{project_path}/Gemfile")
   end
 
+  def self.ruby_command(project_path, opts = {})
+    command = File.exists?("#{project_path}/#{opts[:script]}") ? opts[:script] : opts[:bin]
+    ruby_interpeter = opts[:ruby_interpeter] || "ruby"
+
+    if bundler?(project_path)
+      "#{ruby_interpeter} -S bundle exec #{command}"
+    else
+      "#{ruby_interpeter} -S #{command}"
+    end
+  end
+
 end
--- test/adapters/helpers/test_ruby_env.rb
+++ test/adapters/helpers/test_ruby_env.rb
@@ -26,4 +26,46 @@ class RubyEnvTest < Test::Unit::TestCase
 
   end
 
+  context "self.ruby_command" do
+    
+    should "use ruby by default" do
+      flexmock(RubyEnv).should_receive(:bundler?).and_return(false)
+      flexmock(File).should_receive(:exists?).and_return(false)
+      assert_equal 'ruby -S rspec', RubyEnv.ruby_command("path/to/project", :script => "script/spec", :bin => "rspec")
+    end
+    
+    should "use bundler when available and use the binary when there is no script" do
+      flexmock(RubyEnv).should_receive(:bundler?).once.with("path/to/project").and_return(true)
+      flexmock(File).should_receive(:exists?).with("path/to/project/script/spec").and_return(false)
+      assert_equal 'ruby -S bundle exec rspec', RubyEnv.ruby_command("path/to/project", :script => "script/spec", :bin => "rspec")  
+    end
+
+    should "use the script when it exists when using bundler" do
+      flexmock(RubyEnv).should_receive(:bundler?).and_return(true)
+      flexmock(File).should_receive(:exists?).and_return(true)
+      assert_equal 'ruby -S bundle exec script/spec', RubyEnv.ruby_command("path/to/project", :script => "script/spec", :bin => "rspec")  
+    end
+
+    should "use the script when it exists when not using bundler" do
+      flexmock(RubyEnv).should_receive(:bundler?).and_return(false)
+      flexmock(File).should_receive(:exists?).and_return(true)
+      assert_equal 'ruby -S script/spec', RubyEnv.ruby_command("path/to/project", :script => "script/spec", :bin => "rspec")  
+    end
+
+    should "be able to use jruby" do
+      flexmock(RubyEnv).should_receive(:bundler?).and_return(false)
+      flexmock(File).should_receive(:exists?).and_return(true)
+      assert_equal 'jruby -S script/spec', RubyEnv.ruby_command("path/to/project", :script => "script/spec",
+                                                                :bin => "rspec", :ruby_interpeter => "jruby")
+    end
+
+    should "be able to use jruby with bundler" do
+      flexmock(RubyEnv).should_receive(:bundler?).and_return(true)
+      flexmock(File).should_receive(:exists?).and_return(true)
+      assert_equal 'jruby -S bundle exec script/spec', RubyEnv.ruby_command("path/to/project", :script => "script/spec",
+                                                                            :bin => "rspec", :ruby_interpeter => "jruby")
+    end
+
+  end
+
 end
