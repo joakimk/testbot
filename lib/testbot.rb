40d316992279dc453f3a988fecb8dbb4b484fcfe
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2010-12-04 22:50:37 +0100

    Bumping to 0.4.4.

--- lib/testbot.rb
+++ lib/testbot.rb
@@ -9,7 +9,7 @@ unless defined?(Testbot)
 
     # Don't forget to update readme and changelog
     def self.version
-      version = "0.4.3"
+      version = "0.4.4"
       dev_version_file = File.join(File.dirname(__FILE__), '..', 'DEV_VERSION')
       if File.exists?(dev_version_file)
         version += File.read(dev_version_file)
