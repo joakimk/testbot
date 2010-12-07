21961f48ef0fbef70f96cf0147dde2f926ac14d0
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2010-12-07 23:37:55 +0100

    Pushing out tweaks to rubygems.

--- lib/shared/testbot.rb
+++ lib/shared/testbot.rb
@@ -7,7 +7,7 @@ module Testbot
 
   # Don't forget to update readme and changelog
   def self.version
-    version = "0.4.9"
+    version = "0.5.0"
     dev_version_file = File.join(File.dirname(__FILE__), '..', '..', 'DEV_VERSION')
     if File.exists?(dev_version_file)
       version += File.read(dev_version_file)
