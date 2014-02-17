5f35d8def81962fa68259c85141da9b3ab55b624
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2014-02-17 11:36:25 +0100

    Prep for release.

--- lib/shared/version.rb
+++ lib/shared/version.rb
@@ -1,7 +1,7 @@
 module Testbot
   # Don't forget to update readme and changelog
   def self.version
-    version = "0.7.4"
+    version = "0.7.5"
     dev_version_file = File.join(File.dirname(__FILE__), '..', '..', 'DEV_VERSION')
     if File.exists?(dev_version_file)
       version += File.read(dev_version_file)
