75541304deef67e5f7e755d75dff08009a77fec1
Henrik Nyh <henrik@nyh.se>
2015-02-26 15:53:21 +0100

    Whitespace.

--- lib/shared/ssh_tunnel.rb
+++ lib/shared/ssh_tunnel.rb
@@ -2,19 +2,18 @@ require 'rubygems'
 require 'net/ssh'
 
 class SSHTunnel
-  
   def initialize(host, user, local_port = 2288)
     @host, @user, @local_port = host, user, local_port
   end
-  
+
   def open
     connect
-    
+
     start_time = Time.now
     while true
       break if @up
       sleep 0.5
-      
+
       if Time.now - start_time > 5
         puts "SSH connection failed, trying again..."
         start_time = Time.now
@@ -22,7 +21,7 @@ class SSHTunnel
       end
     end
   end
-  
+
   def connect
     @thread.kill! if @thread
     @thread = Thread.new do
@@ -30,7 +29,6 @@ class SSHTunnel
         ssh.forward.local(@local_port, 'localhost', Testbot::SERVER_PORT)
         ssh.loop {  @up = true }
       end
-    end    
+    end
   end
-  
 end
